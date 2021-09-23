53bcf3991f61543ab69e82a4d567394332d0b3c1
Rafael <rftoledo@uwaterloo.ca>
2021-09-23 17:39:47 -0400

    Add straight arrows separate layout

--- src/browser/modules/D3Visualization/lib/visualization/utils/straightArrow.js
+++ src/browser/modules/D3Visualization/lib/visualization/utils/straightArrow.js
@@ -34,15 +34,46 @@ export default class StraightArrow {
     this.length = centreDistance - (startRadius + endRadius)
 
     this.shaftLength = this.length - headHeight
+    this.separateArrowWidth = 0
     const startArrow = startRadius
     const endShaft = startArrow + this.shaftLength
     const endArrow = startArrow + this.length
     const shaftRadius = shaftWidth / 2
     const headRadius = headWidth / 2
 
+    const separateOutline = (colorCount, index) => {
+      const hLength = 6
+      const distance = Math.min(6, startRadius / colorCount)
+      this.separateArrowWidth = distance * colorCount
+      const sRadius = distance * 0.25
+      const hRadius = distance * 0.4
+
+      var offset
+      if (index === 0 && colorCount > 1) {
+        offset = -Math.floor(colorCount / 2) * distance
+      } else if (index < Math.floor(colorCount / 2)) {
+        offset = -((Math.floor(colorCount / 2) - index) * distance)
+      } else if (index === Math.floor(colorCount / 2) || (index === 0 && colorCount <= 1)) {
+        offset = 0
+      } else if (index > Math.floor(colorCount / 2)) {
+        offset = (index - Math.floor(colorCount / 2)) * distance
+      }
+
+      return [
+        'M', startArrow, sRadius + offset,
+        'L', endShaft + hLength, sRadius + offset,
+        'L', endShaft + hLength, hRadius + offset,
+        'L', endArrow, offset,
+        'L', endShaft + hLength, -hRadius + offset,
+        'L', endShaft + hLength, -sRadius + offset,
+        'L', startArrow, -sRadius + offset,
+        'Z'
+      ].join(' ')
+    }
+
     this.midShaftPoint = layout => ({
       x: startArrow + this.shaftLength / 2,
-      y: layout !== 'stripes' ? -captionHeight * 0.625 - shaftRadius : 0
+      y: layout !== 'stripes' ? -captionHeight * 0.625 - Math.max(shaftRadius, this.separateArrowWidth / 2) : 0
     })
 
     this.outline = function (shortCaptionLength, colorCount, layout) {
@@ -82,6 +113,15 @@ export default class StraightArrow {
         ].join(' ')
       }
       const attrs = {}
+
+      if (layout === 'separate') {
+        return Array(colorCount)
+          .fill()
+          .map((_, i) => ({
+            path: separateOutline(colorCount, i)
+          }))
+      }
+
       if (layout === 'stripes') {
         attrs.x1 = 0
         attrs.y1 = shaftRadius
