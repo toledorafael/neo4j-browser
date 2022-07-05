0fb8e596359b2ecf280a7df19c141c962c6d66f2
Rafael Toledo <rftoledo@uwaterloo.ca>
2022-07-05 19:32:02 -0400

    Add submission form for task answers

---
+++ src/browser/modules/Carousel/NameForm.tsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { useState } from 'react'
+
+// type NameForm = any
+type State = any
+
+export default function NameForm(props: any): JSX.Element {
+  const [value, setValue] = useState('')
+  const [disableButton, setDisableButton] = useState(() => {
+    return localStorage.getItem(props.id) !== ''
+  })
+
+  const handleChange = (event: any) => {
+    setValue(event.target.value)
+  }
+
+  const handleSubmit = (event: any) => {
+    localStorage.setItem(props.id, value)
+    localStorage.setItem(props.id + '-submitTime', String(Date.now()))
+    setValue('')
+    setDisableButton(true)
+    event.preventDefault()
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        <textarea rows={5} cols={50} value={value} onChange={handleChange} />
+      </label>
+      <br />
+      <input type="submit" disabled={disableButton} value="Submit" />
+    </form>
+  )
+}
