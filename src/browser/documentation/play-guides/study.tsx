b01618bb0547f532bf33bbcecb4ce14589d26730
Rafael Toledo <rftoledo@uwaterloo.ca>
2023-03-19 19:52:21 -0400

    Add missing 'k'

--- src/browser/documentation/play-guides/study.tsx
+++ src/browser/documentation/play-guides/study.tsx
@@ -177,7 +177,7 @@ const slides = [
         </h5>
         <ul>
           <li>
-            V1: kWeighted /\ kUndirected /\ kDFS /\ !BFS /\ kCycle /\
+            V1: kWeighted /\ kUndirected /\ kDFS /\ !kBFS /\ kCycle /\
             kConnectedComps /\ !kPrim
           </li>
         </ul>
@@ -205,11 +205,11 @@ const slides = [
         </h5>
         <ul>
           <li>
-            V1: kWeighted /\ kUndirected /\ kDFS /\ !BFS /\ kCycle /\
+            V1: kWeighted /\ kUndirected /\ kDFS /\ !kBFS /\ kCycle /\
             kConnectedComps /\ !kPrim
           </li>
           <li>
-            V2: kWeighted /\ kUndirected /\ !kDFS /\ BFS /\ !kCycle /\
+            V2: kWeighted /\ kUndirected /\ !kDFS /\ kBFS /\ !kCycle /\
             kConnectedComps /\ kPrim
           </li>
         </ul>
@@ -238,11 +238,11 @@ const slides = [
         </h5>
         <ul>
           <li>
-            V1: kWeighted /\ kUndirected /\ kDFS /\ !BFS /\ kCycle /\
+            V1: kWeighted /\ kUndirected /\ kDFS /\ !kBFS /\ kCycle /\
             kConnectedComps /\ !kPrim
           </li>
           <li>
-            V2: kWeighted /\ kUndirected /\ !kDFS /\ BFS /\ !kCycle /\
+            V2: kWeighted /\ kUndirected /\ !kDFS /\ kBFS /\ !kCycle /\
             kConnectedComps /\ kPrim
           </li>
         </ul>
